f47737679a77faafa45eda4c3b3d326989426b01
KissAniko <126045521+KissAniko@users.noreply.github.com>
2023-05-02 10:37:04 +0200

    Add project files.

---
+++ SQLTermekek Console2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient; // MySqlData-t telepíteni kell
+
+
+namespace SQLTermekek_Console2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string kapcsolatleiro = "datasource=127.0.0.1;port=3306;database=hardver;username=root;password=;";
+
+            MySqlConnection SQLkapcsolat = new MySqlConnection(kapcsolatleiro);
+            try
+            {
+                SQLkapcsolat.Open();
+            }
+            catch (MySqlException hiba)
+            {
+                Console.WriteLine(hiba.Message);
+                Environment.Exit(1);
+            }
+
+            //Kategóriák listázása a kategóriában előforduló termékfajták számával 
+            // csak a legalább 100 termékfajtát tartalmazó kategória szerepel
+            //termékfajták száma szerint csökkenő sorrendben. 
+
+            string SQLselect =
+                "SELECT Kategória, COUNT(Kategória) as Darab FROM termékek " +
+                "GROUP BY 'Kategória' " +
+                "HAVING Darab>=100 " +
+                "ORDER BY Darab DESC";
+
+            MySqlCommand SQLparancs = new MySqlCommand(SQLselect, SQLkapcsolat);
+            MySqlDataReader eredmenyOlvaso = SQLparancs.ExecuteReader();
+            while (eredmenyOlvaso.Read())
+            {
+                //Itt már két mező van. Megj. A második egy alias
+                Console.Write(eredmenyOlvaso.GetString("Kategória") + " - ");
+                Console.WriteLine(Convert.ToInt32(eredmenyOlvaso.GetString("Darab")));
+            }
+            eredmenyOlvaso.Close();
+            SQLkapcsolat.Close();
+
+        }
+    }
+}
